2bd0b627073dd62a7d2eee79bc4919a2e97ce602
sfoster <sam@sam-i-am.com>
2012-08-28 16:02:16 +0100

    First step in having Observable stores return ko.observableArray-s. Store change notifications unhooked for now

--- Observable.js
+++ Observable.js
@@ -1,5 +1,5 @@
-define(["./lib/util", "./lib/Promise" /*=====, "./api/Store" =====*/
-], function(lang, Deferred /*=====, Store =====*/){
+define(["lang", "./lib/promise", "knockout" /*=====, "./api/Store" =====*/
+], function(lang, Deferred, ko /*=====, Store =====*/){
 
 // module:
 //		store/Observable
@@ -19,7 +19,7 @@ var Observable = function(/*Store*/ store){
 	//		Create a Memory store that returns an observable query, and then log some
 	//		information about that query.
 	//
-	//	|	var store = store.Observable(new store.Memory({
+	//	| var store = store.Observable(new store.Memory({
 	//	|		data: [
 	//	|			{id: 1, name: "one", prime: false},
 	//	|			{id: 2, name: "two", even: true, prime: true},
@@ -27,11 +27,11 @@ var Observable = function(/*Store*/ store){
 	//	|			{id: 4, name: "four", even: true, prime: false},
 	//	|			{id: 5, name: "five", prime: true}
 	//	|		]
-	//	|	}));
-	//	|	var changes = [], results = store.query({ prime: true });
-	//	|	var observer = results.observe(function(object, previousIndex, newIndex){
+	//	| }));
+	//	| var changes = [], results = store.query({ prime: true });
+	//	| var observer = results.observe(function(object, previousIndex, newIndex){
 	//	|		changes.push({previousIndex:previousIndex, newIndex:newIndex, object:object});
-	//	|	});
+	//	| });
 	//
 	//		See the Observable tests for more information.
 
@@ -39,7 +39,7 @@ var Observable = function(/*Store*/ store){
 	// a Comet driven store could directly call notify to notify observers when data has
 	// changed on the backend
 	// create a new instance
-	store = lang.create(store);
+	store = Object.create(store);
 	
 	store.notify = function(object, existingId){
 		revision++;
@@ -52,93 +52,104 @@ var Observable = function(/*Store*/ store){
 	store.query = function(query, options){
 		options = options || {};
 		var results = originalQuery.apply(this, arguments);
+		// our return value - a knockout-js Observable
+		var observedResults = ko.observableArray(results);
+		
 		if(results && results.forEach){
-			var nonPagedOptions = lang.mixin({}, options);
-			delete nonPagedOptions.start;
-			delete nonPagedOptions.count;
-
-			var queryExecutor = store.queryEngine && store.queryEngine(query, nonPagedOptions);
-			var queryRevision = revision;
-			var listeners = [], queryUpdater;
-			results.observe = function(listener, includeObjectUpdates){
-				if(listeners.push(listener) == 1){
-					// first listener was added, create the query checker and updater
-					queryUpdaters.push(queryUpdater = function(changed, existingId){
-						Deferred.when(results, function(resultsArray){
-							var atEnd = resultsArray.length != options.count;
-							var i, l, listener;
-							if(++queryRevision != revision){
-								throw new Error("Query is out of date, you must observe() the query prior to any data modifications");
-							}
-							var removedObject, removedFrom = -1, insertedInto = -1;
-							if(existingId !== undef){
-								// remove the old one
-								for(i = 0, l = resultsArray.length; i < l; i++){
-									var object = resultsArray[i];
-									if(store.getIdentity(object) == existingId){
-										removedObject = object;
-										removedFrom = i;
-										if(queryExecutor || !changed){// if it was changed and we don't have a queryExecutor, we shouldn't remove it because updated objects would be eliminated
-											resultsArray.splice(i, 1);
-										}
-										break;
-									}
-								}
-							}
-							if(queryExecutor){
-								// add the new one
-								if(changed &&
-										// if a matches function exists, use that (probably more efficient)
-										(queryExecutor.matches ? queryExecutor.matches(changed) : queryExecutor([changed]).length)){
-
-									var firstInsertedInto = removedFrom > -1 ? 
-										removedFrom : // put back in the original slot so it doesn't move unless it needs to (relying on a stable sort below)
-										resultsArray.length;
-									resultsArray.splice(firstInsertedInto, 0, changed); // add the new item
-									insertedInto = queryExecutor(resultsArray).indexOf(changed); // sort it
-									// we now need to push the chagne back into the original results array
-									resultsArray.splice(firstInsertedInto, 1); // remove the inserted item from the previous index
-									
-									if((options.start && insertedInto == 0) ||
-										(!atEnd && insertedInto == resultsArray.length)){
-										// if it is at the end of the page, assume it goes into the prev or next page
-										insertedInto = -1;
-									}else{
-										resultsArray.splice(insertedInto, 0, changed); // and insert into the results array with the correct index
-									}
-								}
-							}else if(changed && !options.start){
-								// we don't have a queryEngine, so we can't provide any information
-								// about where it was inserted, but we can at least indicate a new object
-								insertedInto = removedFrom >= 0 ? removedFrom : (store.defaultIndex || 0);
-							}
-							if((removedFrom > -1 || insertedInto > -1) &&
-									(includeObjectUpdates || !queryExecutor || (removedFrom != insertedInto))){
-								var copyListeners = listeners.slice();
-								for(i = 0;listener = copyListeners[i]; i++){
-									listener(changed || removedObject, removedFrom, insertedInto);
-								}
-							}
-						});
-					});
-				}
-				var handle = {};
-				// TODO: Remove cancel in 2.0.
-				handle.remove = handle.cancel = function(){
-					// remove this listener
-					var index = listeners.indexOf(listener);
-					if(index > -1){ // check to make sure we haven't already called cancel
-						listeners.splice(index, 1);
-						if(!listeners.length){
-							// no more listeners, remove the query updater too
-							queryUpdaters.splice(queryUpdaters.indexOf(queryUpdater), 1);
-						}
-					}
-				};
-				return handle;
-			};
+			// TODO: reinstate the queryUpdates functionality, 
+			// which allows subscribers to relevant queries to be notified when a store change 
+			// implies a change in the results for a query
+			
+			// var nonPagedOptions = lang.mixin({}, options);
+			// delete nonPagedOptions.start;
+			// delete nonPagedOptions.count;
+			// 
+			// var queryExecutor = store.queryEngine && store.queryEngine(query, nonPagedOptions);
+			// var queryRevision = revision;
+			// var listeners = [], queryUpdater;
+			// results.observe = function(listener, includeObjectUpdates){
+			//	if(listeners.push(listener) == 1){
+			//		// first listener was added, create the query checker and updater
+			//		queryUpdaters.push(queryUpdater = function(changed, existingId){
+			//			Deferred.when(results, function(resultsArray){
+			//				var atEnd = resultsArray.length != options.count;
+			//				var i, l, listener;
+			//				if(++queryRevision != revision){
+			//					throw new Error("Query is out of date, you must observe() the query prior to any data modifications");
+			//				}
+			//				var removedObject, removedFrom = -1, insertedInto = -1;
+			//				if(existingId !== undef){
+			//					// remove the old one
+			//					for(i = 0, l = resultsArray.length; i < l; i++){
+			//						var object = resultsArray[i];
+			//						if(store.getIdentity(object) == existingId){
+			//							removedObject = object;
+			//							removedFrom = i;
+			//							if(queryExecutor || !changed){// if it was changed and we don't have a queryExecutor, we shouldn't remove it because updated objects would be eliminated
+			//								resultsArray.splice(i, 1);
+			//							}
+			//							break;
+			//						}
+			//					}
+			//				}
+			//				if(queryExecutor){
+			//					// add the new one
+			//					if(changed &&
+			//							// if a matches function exists, use that (probably more efficient)
+			//							(queryExecutor.matches ? queryExecutor.matches(changed) : queryExecutor([changed]).length)){
+			// 
+			//						var firstInsertedInto = removedFrom > -1 ? 
+			//							removedFrom : // put back in the original slot so it doesn't move unless it needs to (relying on a stable sort below)
+			//							resultsArray.length;
+			//						resultsArray.splice(firstInsertedInto, 0, changed); // add the new item
+			//						insertedInto = queryExecutor(resultsArray).indexOf(changed); // sort it
+			//						// we now need to push the chagne back into the original results array
+			//						resultsArray.splice(firstInsertedInto, 1); // remove the inserted item from the previous index
+			//						
+			//						if((options.start && insertedInto == 0) ||
+			//							(!atEnd && insertedInto == resultsArray.length)){
+			//							// if it is at the end of the page, assume it goes into the prev or next page
+			//							insertedInto = -1;
+			//						}else{
+			//							resultsArray.splice(insertedInto, 0, changed); // and insert into the results array with the correct index
+			//						}
+			//					}
+			//				}else if(changed && !options.start){
+			//					// we don't have a queryEngine, so we can't provide any information
+			//					// about where it was inserted, but we can at least indicate a new object
+			//					insertedInto = removedFrom >= 0 ? removedFrom : (store.defaultIndex || 0);
+			//				}
+			//				if((removedFrom > -1 || insertedInto > -1) &&
+			//						(includeObjectUpdates || !queryExecutor || (removedFrom != insertedInto))){
+			//					var copyListeners = listeners.slice();
+			//					for(i = 0;listener = copyListeners[i]; i++){
+			//						listener(changed || removedObject, removedFrom, insertedInto);
+			//					}
+			//				}
+			//			});
+			//		});
+			//	}
+			//	var handle = {};
+			//	// TODO: Remove cancel in 2.0.
+			//	handle.remove = handle.cancel = function(){
+			//		// remove this listener
+			//		var index = listeners.indexOf(listener);
+			//		if(index > -1){ // check to make sure we haven't already called cancel
+			//			listeners.splice(index, 1);
+			//			if(!listeners.length){
+			//				// no more listeners, remove the query updater too
+			//				queryUpdaters.splice(queryUpdaters.indexOf(queryUpdater), 1);
+			//			}
+			//		}
+			//	};
+			//	return handle;
+			// };
 		}
-		return results;
+		Deferred.when(results, function(arrayResults){
+		// put results in there and hook up subscribers when we have them
+			observedResults.splice.apply(observedResults, [0, arrayResults.length].concat(arrayResults));
+		});
+		return observedResults;
 	};
 	var inMethod;
 	function whenFinished(method, action){

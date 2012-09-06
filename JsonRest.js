5b014761a6af25d13b162c57a208a883afc25fe3
sfoster <sam@sam-i-am.com>
2012-09-06 22:24:15 +0100

    Decorate the promise from $.ajax with the xhr so we can get response headers from it later

--- JsonRest.js
+++ JsonRest.js
@@ -167,16 +167,22 @@ lang.mixin(JsonRest.prototype, {
 				query += ")";
 			}
 		}
+
+		var resultsXhr = null;
 		var results = $.ajax({
 			type: "GET",
+			beforeSend: function(xhr){
+			  resultsXhr = xhr; // XMLHttpRequest object
+			},
 			url: this.target + (query || ""),
 			dataType: "json",
 			headers: headers
 		});
 		results.total = results.then(function(){
-			var range = results.xhr.getResponseHeader("Content-Range");
+			var range = (results.xhr || resultsXhr).getResponseHeader("Content-Range");
 			return range && (range = range.match(/\/(.*)/)) && +range[1];
 		});
+
 		return QueryResults(results);
 	}
 });
